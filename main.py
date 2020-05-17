13e98cd306ba81295e732c38f23d1366401b92b1
yusfer <yustasfernando@gmail.com>
2020-05-17 11:14:16 +0200

    Puntuación sin finalizar

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # add some more after the game creation
 
 import random
+import pandas as pd
 
 # switcher function to translate number-to-value
 def switch_values(num):
@@ -24,14 +25,17 @@ def randomvalue():
 
 # compare if value is a right option for the game
 # modify "cont" boolean for finish the loop
-def is_right(val):
+def is_right(val,):
 	if val=="Rock" or val=="Paper" or val=="Scissor":
+		return False
+	elif val == "exit":
 		return False
 	else:
 		print ("Please, insert a right option for the game")
+		
 		return True
 
-def compare_value(p1,p2):
+def compare_value(p1,p2,punt):
 
 	print("%s :::: %s" %(p1.player_name,p1.chose))
 	print("%s :::: %s" %(p2.player_name,p2.chose))
@@ -41,10 +45,34 @@ def compare_value(p1,p2):
 
 	if val1 == val2:
 		print ("EMPATE")
+		punt["P1"].append("-")
+		punt["CPU"].append("-")
 	elif (val1=="Rock" and val2=="Scissor") or (val1=="Paper" and val2=="Rock") or(val1=="Scissor" and val2=="Paper"):
 		print ("WIN FOR %s " %p1.player_name)
+		punt["P1"].append("X")
+		punt["CPU"].append("-")
 	else:
 		print ("WIN FOR %s " %p2.player_name)
+		punt["P1"].append("-")
+		punt["CPU"].append("X")
+
+def sum_punt(table):
+	punt1 = 0
+	punt2 = 0
+
+	for num in table["P1"]:
+		print(num)
+		if num == "X":
+			punt1 += 1
+
+	for num2 in table["CPU"]:
+		print(num2)
+		if num2 == "X":
+			punt2 += 1
+
+	table["P1"].append(punt1)
+	table["CPU"].append(punt2)
+
 
 class player:
 	player_name=""
@@ -61,26 +89,42 @@ class player:
 print("Please, enter your name:")
 player1 = player(input(),"")
 
+punctuation = {
+	"P1":[],
+	"CPU":[]
+}
 # Take the election from player usin command line
 # if value is not right, repeat and repeat
 
-#this infinite loop is for repeat the game forever and ever
-while True:
+#this infinite loop is for repeat the game forever and ever until player write "exit"
+exit = False
+while not exit:
 	execute = True
+	#that loop is executing until value is right
 	while execute:
-		print (" Rock, Paper or Scissor?")
+		print (" ---Rock, Paper or Scissor? (exit for leave)")
 		#player_chosen = input().capitalize()
 		player1.chose = input().capitalize()
+
+		#if player want to leave, break execution loop
+		if player1.chose == "Exit":
+			exit = True
+			break
+
 		execute=is_right(player1.chose)
 
-	# election for CPU
-	cpu = player("CPU", randomvalue())
-	#cpu_chosen = randomvalue()
+	#if player want to leave, dont generate value for CPU
 
-	#print ("Your selection is %s" %player1.chose)
-	#print ("CPU selection is %s" %cpu.chose)
-
-
-	# comparation of values
-	compare_value(player1,cpu)
+	if not exit:
+			# election for CPU and comparation
+		cpu = player("CPU", randomvalue())
+		compare_value(player1,cpu,punctuation)
+	else:
+		print(punctuation)
+		sum_punt(punctuation)
+		punctuation["P1"].append("total del 1")
+		punctuation["CPU"].append("total del CPU")
+		punt_table = pd.DataFrame(punctuation)
+		print(punt_table)
+		print("See you soon!")
 
